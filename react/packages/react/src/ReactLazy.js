37ffee2b80b2e3eb212f5b58de354a720718ba5f
Vina <707114574@qq.com>
2019-10-08 18:42:03 +0800

    react code

---
+++ react/packages/react/src/ReactLazy.js
@@ -0,0 +1,20 @@
+/**
+ * Copyright (c) Facebook, Inc. and its affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+import type {LazyComponent, Thenable} from 'shared/ReactLazyComponent';
+
+import {REACT_LAZY_TYPE} from 'shared/ReactSymbols';
+
+export function lazy<T, R>(ctor: () => Thenable<T, R>): LazyComponent<T> {
+  return {
+    $$typeof: REACT_LAZY_TYPE,
+    _ctor: ctor,  //传进来的防范
+    // React uses these fields to store the result.
+    _status: -1,  //promise加载数据的不同状态会变
+    _result: null,
+  };
+}
